24105ba75055e61cbcbf727507beeb40452175b1
Kishansoni3739 <kishansoni8403@gmail.com>
2026-05-24 11:47:05 +0530

    Fix CORS

--- server.js
+++ server.js
@@ -409,7 +409,12 @@ function validateStateShape(body) {
 
 const app = express();
 app.use(cors({
-  origin:process.env.FRONTEND_URL
+  origin:[
+    process.env.FRONTEND_URL,
+    "http://localhost",
+    "capacitor://localhost"
+  ]
+
 }));
 app.use(express.json({ limit: "12mb" }));
 
